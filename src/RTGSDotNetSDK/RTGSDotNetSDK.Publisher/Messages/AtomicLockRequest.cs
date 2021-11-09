3e29beee594c45f3d704f26ac3f71f310ac44801
Joe Reilly <joseph.reilly@rtgs.com>
2021-11-09 16:52:56 +0000

    2776 - WIP

---
+++ src/RTGSDotNetSDK/RTGSDotNetSDK.Publisher/Messages/AtomicLockRequest.cs
@@ -0,0 +1,13 @@
+namespace RTGSDotNetSDK.Publisher
+{
+	public record AtomicLockRequest
+	{
+		// TODO: properties
+		public GenericFinancialIdentification1 DbtrToRtgsId { get; init; }
+		public ActiveCurrencyAndAmount CdtrAmt { get; init; }
+		public CashAccount38 UltmtDbtrAcct { get; init; }
+		public CashAccount38 UltmtCdtrAcct { get; init; }
+		public string SplmtryData { get; init; }
+		public string EndToEndId { get; init; }
+	}
+}
