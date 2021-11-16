743741ad0ecd9124b339796419943fbcc5fc1af0
Joe Reilly <joseph.reilly@rtgs.com>
2021-11-16 15:22:08 +0000

    Unit tests WIP

---
+++ src/RTGS.DotNetSDK/RTGS.DotNetSDK.Publisher.Tests/GivenRtgsPublisher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using RTGS.DotNetSDK.Publisher
+
+namespace RTGS.DotNetSDK.Publisher.Tests
+{
+    public class GivenRtgsPublisher
+    {
+		public GivenRtgsPublisher()
+		{
+			var rtgsPublisher = new RtgsPublisher
+		}
+
+		[Fact]
+		public void WhenToRtgsCallInitialisationFails_ThenLogFailure()
+		{
+
+		}
+    }
+}
