819a543c4e9c2ca1abd6fcfec17aca7a891a1c31
Darren <dosborne@igale.co.uk>
2021-11-24 10:29:52 +0000

    work in progress

--- src/RTGS.DotNetSDK/RTGS.DotNetSDK.Publisher/Extensions/ServiceCollectionExtensions.cs
+++ src/RTGS.DotNetSDK/RTGS.DotNetSDK.Publisher/Extensions/ServiceCollectionExtensions.cs
@@ -6,6 +6,13 @@ namespace RTGS.DotNetSDK.Publisher.Extensions
 {
 	public static class ServiceCollectionExtensions
 	{
+		/// <summary>
+		/// Adds a publisher to the service collection.
+		/// </summary>
+		/// <param name="serviceCollection">The service collection</param>
+		/// <param name="options">Client options</param>
+		/// <param name="configureGrpcClient">Grpc client configuration</param>
+		/// <returns>IServiceCollection - The IServiceCollection so that additional calls can be chained.</returns>
 		public static IServiceCollection AddRtgsPublisher(
 			this IServiceCollection serviceCollection,
 			RtgsClientOptions options,
