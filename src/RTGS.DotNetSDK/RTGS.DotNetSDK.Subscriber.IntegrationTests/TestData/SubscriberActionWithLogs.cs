4d4ed7bcb5e6bb4ef07cafae5ef6625f2f95a0c5
Lewis Henson <lewis.henson@gmail.com>
2021-12-09 15:23:41 +0000

    Fix format issues

--- src/RTGS.DotNetSDK/RTGS.DotNetSDK.Subscriber.IntegrationTests/TestData/SubscriberActionWithLogs.cs
+++ src/RTGS.DotNetSDK/RTGS.DotNetSDK.Subscriber.IntegrationTests/TestData/SubscriberActionWithLogs.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using RTGS.DotNetSDK.Subscriber.Handlers;
 using RTGS.DotNetSDK.Subscriber.IntegrationTests.Logging;
 using RTGS.DotNetSDK.Subscriber.IntegrationTests.TestHandlers;
--- src/RTGS.DotNetSDK/RTGS.DotNetSDK.Subscriber.IntegrationTests/TestData/SubscriberActionWithLogsData.cs
+++ src/RTGS.DotNetSDK/RTGS.DotNetSDK.Subscriber.IntegrationTests/TestData/SubscriberActionWithLogsData.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using RTGS.DotNetSDK.Subscriber.IntegrationTests.Logging;
 using RTGS.DotNetSDK.Subscriber.Messages;
 using RTGS.ISO20022.Messages.Admi_002_001.V01;
