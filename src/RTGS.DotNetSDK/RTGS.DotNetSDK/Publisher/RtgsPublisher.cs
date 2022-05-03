fa7a18092039bff6a29c678903e5ac94525df86c
Lewis Henson <lewishenson@users.noreply.github.com>
2022-05-03 18:01:22 +0100

    Use messages from RTGS.Public.Messages package

--- src/RTGS.DotNetSDK/RTGS.DotNetSDK/Publisher/RtgsPublisher.cs
+++ src/RTGS.DotNetSDK/RTGS.DotNetSDK/Publisher/RtgsPublisher.cs
@@ -1,7 +1,7 @@
-﻿using RTGS.DotNetSDK.Publisher.Messages;
-using RTGS.ISO20022.Messages.Admi_002_001.V01;
+﻿using RTGS.ISO20022.Messages.Admi_002_001.V01;
 using RTGS.ISO20022.Messages.Camt_054_001.V09;
 using RTGS.ISO20022.Messages.Pacs_008_001.V10;
+using RTGS.Public.Messages.Publisher;
 
 namespace RTGS.DotNetSDK.Publisher;
 
